9b7529245c7cb56b19d40d6d5bf16e8b8a38079f
Guilherme Baptista <guisgb13@gmail.com>
2011-12-19 11:49:04 -0300

    Small bug fix in PrepareEnvironment.
    Something seemed to mess up when '\n' was being directly appended to
    sudoers stream.
    We now call a processBuilder which calls a shell (/bin/sh -c) that
    executes
    'echo ${filesToGrantPermission} >> ${permissionFile}'

--- src/org/ourgrid/virt/strategies/LinuxUtils.java
+++ src/org/ourgrid/virt/strategies/LinuxUtils.java
@@ -1,25 +1,22 @@
 package org.ourgrid.virt.strategies;
 
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 
 import org.apache.commons.io.IOUtils;
-import org.ourgrid.virt.model.ExecutionResult;
 
 public class LinuxUtils {
 
 	public static final String LINUX_PERMISSIONS_FILE = "/etc/sudoers";
-	
+
 	public static void appendLineToSudoersFile(String userName, String line)
 			throws IOException, FileNotFoundException {
 		String sudoers = IOUtils.toString(new FileReader(LINUX_PERMISSIONS_FILE));
-		String permissionLine = "\n" + userName + " ALL=NOPASSWD: " + line;
-		
+		String permissionLine = userName + " ALL=NOPASSWD: " + line;
+
 		if ( !sudoers.contains(permissionLine) ){
-			
-			ProcessBuilder sudoersAppendProc = new ProcessBuilder("/bin/echo",permissionLine, ">>", LINUX_PERMISSIONS_FILE);
+			ProcessBuilder sudoersAppendProc = new ProcessBuilder("/bin/bash", "-c", "/bin/echo " + permissionLine + " >> " + LINUX_PERMISSIONS_FILE);
 			try {
 				HypervisorUtils.runAndCheckProcess(sudoersAppendProc);
 			} catch (Exception e) {
@@ -28,5 +25,5 @@ public class LinuxUtils {
 			}
 		}
 	}
-	
+
 }
