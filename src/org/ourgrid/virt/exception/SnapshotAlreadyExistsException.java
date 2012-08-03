094d40c8322f17c8cfe31cf75b23e194cb5e0620
marcosnobrega <marcosnobregajr@gmail.com>
2012-08-03 14:36:44 -0300

    Adding generated UID

--- src/org/ourgrid/virt/exception/SnapshotAlreadyExistsException.java
+++ src/org/ourgrid/virt/exception/SnapshotAlreadyExistsException.java
@@ -2,6 +2,11 @@ package org.ourgrid.virt.exception;
 
 public class SnapshotAlreadyExistsException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3716356414047476740L;
+
 	public SnapshotAlreadyExistsException(String message){
 		super( message );
 	}
